e49e162a14daebf07b5e80d3cf0151d0a7fe5e00
Yifan Li <yifan.li9337@gmail.com>
2024-07-04 18:53:18 +1000

    check point - before separating extraction of title

--- main.py
+++ main.py
@@ -1,9 +1,76 @@
-from src.scrap_data import (
-    scrape_resume_link,
-    get_resume_info,
+# from src.scrap_data import (
+#     scrape_resume_link,
+#     get_resume_info,
+# )
+
+from src.sampler import (
+    extract_job_attribute,
 )
 
 
+import os 
+import yaml
+import pandas as pd
+from tqdm import tqdm
+from datetime import datetime
+
+
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+
+CONFIG = yaml.load(
+    open(os.path.join(os.getcwd(), "config.yaml"), "r+"),
+    Loader = yaml.FullLoader
+)
+
+JOB_POSTING_PATH = os.path.join(os.getcwd(), "data", "job_postings_dev_debug.csv")
+FLAN_T5_MODEL_NAME = "google/flan-t5-large"
 
 if __name__ == "__main__":
-    pass
+    
+    job_posting_df = pd.read_csv(JOB_POSTING_PATH)
+
+    tokenizer = T5Tokenizer.from_pretrained(FLAN_T5_MODEL_NAME)
+    model = T5ForConditionalGeneration.from_pretrained(FLAN_T5_MODEL_NAME)
+
+    job_postings_desc_col = job_posting_df["description"]
+    job_title_col = job_posting_df["title"]
+    
+    job_family_col = []
+    level_of_experience_col = []
+    years_of_experience_col = []
+    required_skills_col = []
+
+    for title, desc in tqdm(zip(job_title_col, job_postings_desc_col)):
+        # TODO: add confidence scoring to filter out bad answers
+        job_fam, years, level, skill = extract_job_attribute(
+            job_post_text= f"job title: {title}. description: {desc}",
+            job_family_options=CONFIG["job_family"],
+            level_of_experience_options=CONFIG["experience_level"],
+            definitions_loc=CONFIG["definitions_loc"],
+            model=model,
+            tokenizer=tokenizer
+        )
+        job_family_col.append(job_fam)
+        level_of_experience_col.append(level)
+        years_of_experience_col.append(years)
+        required_skills_col.append(skill)
+
+    job_posting_df_extracted = job_posting_df[["job_id", "title", "description"]]
+    job_posting_df_extracted["job_family"] = job_family_col
+    job_posting_df_extracted["level"] = level_of_experience_col
+    job_posting_df_extracted["years_of_experience"] = years_of_experience_col
+    job_posting_df_extracted["skills"] = required_skills_col
+
+    try:
+        job_posting_df_extracted.to_csv(
+            os.path.join(os.getcwd(), "data", "job_posting_transformed.csv"),
+            index=False
+        )
+    except PermissionError:
+        time_stamp = current_time = datetime.now().strftime('%Y%m%d%H%M%S')
+        job_posting_df_extracted.to_csv(
+            os.path.join(os.getcwd(), "data", f"job_posting_transformed_{time_stamp}.csv"),
+            index=False
+        )
+
--- src/sampler.py
+++ src/sampler.py
@@ -8,6 +8,7 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 import pandas as pd
 from itertools import product
 import string
+import os
 
 # model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
@@ -19,24 +20,34 @@ def initialise_job_exp_grid(job_family_list:list, level_of_experience_list:list)
     )
     return grid_df
 
-def validate_output(answer:str, choices:dict):
-    return answer.upper() in choices.keys()
+def validate_output(answer:str, choices:dict|list):
+    if isinstance(choices, list):
+        return answer.upper() in choices
+    else:
+        return answer.upper() in choices.keys()
 
 def query_flan_t5(
     model:T5ForConditionalGeneration,
     tokenizer:T5Tokenizer,
     context:str,
     question:str,
-    choices:dict = None, # mapping of options [A-Z]{1} -> some text
+    choices:dict | list = None, # mapping of options [A-Z]{1} -> some text
     return_int:bool = False,
+    num_try:int=0,
+    max_try:int=3
 ):
-    input_text = f"question: {question} context: {context}"
+    
     if choices is not None:
-        choices_txt = " ".join([
-            key + ") " + val
-            for key, val in choices.items()
-        ])
-        input_text += f" choices: {' '.join(choices_txt)}"
+        if isinstance(choices, list):
+            choices_txt = ", ".join(choices)
+        else:
+            choices_txt = " ".join([
+                f"{key}) + {val}"
+                for key, val in choices.items()
+            ])
+        input_text = f"question: {question} choices: {choices_txt} context: {context}"
+    else:
+        input_text = f"question: {question} context: {context}"
     inputs = tokenizer(input_text, return_tensors='pt')
 
     # Generate the answer
@@ -44,16 +55,35 @@ def query_flan_t5(
     answer = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
     if choices is not None:                     # if multiple choice
-        if validate_output(answer, choices):    # check if predicted option is one of the available options
-            return choices[answer.upper()]      # return the value of predicted option (expect flan-t5 to predict just A for option A)
+        if validate_output(answer, choices): 
+            if isinstance(choices, list):
+                return answer.upper()
+            else:  
+                return choices[answer.upper()]      # return the value of predicted option (expect flan-t5 to predict just A for option A)
         else:
-            return None 
+            if num_try < max_try:
+                conversion_question = f"Please choose a single letter to represent the context."
+                cleaned_answer:str = query_flan_t5(
+                    model, tokenizer, context=answer, question=conversion_question, choices=list(choices.keys()), return_int=True, num_try=num_try+1
+                )
+                if num_try == 0: # initial layer
+                    return choices[cleaned_answer.upper()]
+                else:
+                    return cleaned_answer.upper()
+            else:
+                return None 
     else:
         if return_int:
             try:
                 return str(int(answer))
             except:
-                raise TypeError("Requested to return integer. prediction cannot be converted ot integer")
+                if num_try < max_try:
+                    conversion_question = "Please return a single integer to represent this range"
+                    return query_flan_t5(
+                        model, tokenizer, context=answer, question=conversion_question, return_int=True, num_try=num_try+1
+                    )
+                else:
+                    return None
         return answer                           # if not multiple choice, return the predicted value directly
 
 def build_choices_dict(options_list:list[str], incl_others:bool=True):
@@ -66,23 +96,28 @@ def extract_job_attribute(
     job_post_text:str, 
     job_family_options:list[str], 
     level_of_experience_options:list[str],
+    definitions_loc:dict,
     model:T5ForConditionalGeneration,
     tokenizer:T5Tokenizer,
 ):
     """return job family, level of experience, minimum years of experience"""
-    
-    q1_question = "What is the job family described in this job post?"
+
+    q1_question = "What is the job family described in the job title and job description?"
     q1_choices = build_choices_dict(job_family_options)
     q1_answer = query_flan_t5(model, tokenizer, job_post_text, q1_question, q1_choices)
 
     q2_question = "What is the minimum number of years of experience required in this job post answer as an integer?"
     q2_answer = query_flan_t5(model, tokenizer, job_post_text, q2_question, return_int=True)
 
-    q3_question = "What level of experience is described in this job posting?"
+    q3_question = "What level of experience is indicated by the responsibility described in this job posting?"
     q3_choices = build_choices_dict(level_of_experience_options)
-    q3_answer = query_flan_t5(model, tokenizer, job_post_text, q3_question, q3_choices)
+    definitions = open(os.path.join(os.getcwd(), *definitions_loc["experience_level"])).read() 
+    q3_answer = query_flan_t5(model, tokenizer, definitions+job_post_text, q3_question, q3_choices)
 
-    return q1_answer, q2_answer, q3_answer
+    q4_question = "What skills does the job applicant must have for this role? do not include qualification and years of experience."
+    q4_answer = query_flan_t5(model, tokenizer, job_post_text, q4_question)
+
+    return q1_answer, q2_answer, q3_answer, q4_answer
 
 def find_job_posting(job_family:str, level_of_experience:str, job_posting_df) -> list:
     """Return a list of job_id"""
