04c8eb0fc709c8aa531e95d8fb8b517da089c730
Yifan Li <yifan.li9337@gmail.com>
2024-07-09 20:19:27 +1000

    successfully crawled 800 resumes

--- src/scrape_resume_data.py
+++ src/scrape_resume_data.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.common.by import By
+from fake_useragent import UserAgent
 
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 import pandas as pd
@@ -11,10 +12,12 @@ import bs4
 import requests
 import re
 import hashlib
+import time
 
 pattern_job_title = r'.+(?=\sresume example with)'
 pattern_num_year = r'(?<=resume example with\s)[0-9]+'
 pattern = re.compile(r'^SECTION_(SUMM|HILT|EDUC|EXPR|SKLL).+$')
+pattern_skill_exp = re.compile(r'^SECTION_(EXPR|SKLL).+$')
 
 def id(x):
     return int(hashlib.md5(x.encode('utf-8')).hexdigest(), 16)
@@ -25,6 +28,9 @@ def set_up_driver(headless:bool=False) -> webdriver.Edge:
         edge_options.add_argument("--headless")  
         edge_options.add_argument("--disable-gpu")  
         edge_options.add_argument("--window-size=1920x1080") 
+    ua = UserAgent()
+    userAgent = ua.random
+    edge_options.add_argument(f'user-agent={userAgent}')
     service = Service(EdgeChromiumDriverManager().install())
     driver = webdriver.Edge(service=service, options=edge_options)
     return driver 
@@ -63,18 +69,28 @@ def set_up_driver(headless:bool=False) -> webdriver.Edge:
 # df["link"] = link
 # df["id"] = df["link"].apply(id)
 
-def scrape_resume_link(driver:webdriver.Edge, job_family:str, exp_level:str, min_rating:int=85, max_rating:int=100, max_page:int=5):
+def scrape_resume_link(
+    job_family:str, 
+    exp_lower:str, 
+    exp_upper:str,
+    min_rating:int=85, 
+    max_rating:int=100, 
+    max_page:int=5
+):
     """Return a list of resume links matching the search criteria"""
     result = []
-    search_keyword = " ".join([exp_level, job_family]).lower().replace(" ","%20")
-    for i in range(max_page):   
+    search_keyword = job_family.lower().replace(" ","%20")
+    for i in range(max_page): 
         page_no = str(i+1)
-        url = f"https://www.livecareer.com/resume-search/search?jt={search_keyword}&bg={str(min_rating)}&eg={str(max_rating)}&comp=&mod=&pg={page_no}"
+        url = f"https://www.livecareer.com/resume-search/search?jt={search_keyword}&be={exp_lower}&ee={exp_upper}&bg={str(min_rating)}&eg={str(max_rating)}&comp=&mod=&pg={page_no}"
+        driver=set_up_driver()
         driver.get(url)
+        time.sleep(0.1)
         a_tags_in_div = driver.find_elements(By.CSS_SELECTOR, 'div a') 
         for a in a_tags_in_div:
             if a.get_attribute('class') == "sc-1dzblrg-0 caJIKu sc-1os65za-2 jhoVRR":
                 result.append(a.get_attribute('href'))
+        driver.quit()
     return result
 
 def get_resume_info(driver:webdriver.Edge, link:str):
@@ -84,17 +100,23 @@ def get_resume_info(driver:webdriver.Edge, link:str):
         # tmp_years = '' 
         # tmp_job_title = ''
         driver.get(link)
+        time.sleep(0.1)
         html_content = driver.page_source
         soup = BeautifulSoup(html_content, 'html.parser')
+        title_tag = soup.find('title')
+        if title_tag and '404 - Page Not Found' in title_tag.text:
+            return None, None, None, None
         divs = soup.find_all('div', id=pattern)
         tmp_resume = "\n".join([div.get_text(strip=True)for div in divs])
+        divs_skill_exp = soup.find_all('div', id=pattern_skill_exp)
+        tmp_skill_exp = "\n".join([div.get_text(strip=True)for div in divs_skill_exp])
         page_title = soup.find_all('h2', class_='title')[0].get_text(strip=False)
         tmp_job_title = re.findall(pattern_job_title, page_title)[0]
         tmp_years = re.findall(pattern_num_year, page_title)[0]
-    except Exception:
-        return None, None, None
-    finally:
-        return tmp_job_title, tmp_resume, tmp_years
+        return tmp_job_title, tmp_resume, tmp_years, tmp_skill_exp
+    except Exception as e:
+        return None, None, None, None
+        
 
 # for l in df['link']:
 #     try:        
--- subprocess_scrape_livecareer.py
+++ subprocess_scrape_livecareer.py
@@ -11,6 +11,7 @@ from src.sampler import (
     extract_years_of_experience,
 )
 from src.scrape_resume_data import (
+    set_up_driver,
     scrape_resume_link,
     get_resume_info
 )
@@ -40,6 +41,8 @@ from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.common.by import By
+import json
+from selenium_stealth import stealth
 
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 import pandas as pd
@@ -49,7 +52,7 @@ import bs4
 import requests
 import re
 import hashlib
-
+import random
 pattern_job_title = r'.+(?=\sresume example with)'
 pattern_num_year = r'(?<=resume example with\s)[0-9]+'
 pattern = re.compile(r'^SECTION_(SUMM|HILT|EDUC|EXPR|SKLL).+$')
@@ -67,32 +70,62 @@ def scrape_resume():
     exp_list = [(val['start'], val['end']) for _, val in CONFIG['resume_scraping_params']['experience_band'].items()]
     grid = initialise_job_exp_grid(job_fam_list, exp_list)
 
+    links = []
+    for i in tqdm(range(grid.shape[0]), "Scraping", ncols=100, total=grid.shape[0]):
+        job_fam = str(grid['job_family'][i])
+        exp_lower = str(grid['exp_lower'][i])
+        exp_upper = str(grid['exp_upper'][i])
+        logger.debug(f"scraping for {job_fam} between {exp_lower} and {exp_upper} years of experience")
+        tmp_list = scrape_resume_link(
+            job_family=job_fam,
+            exp_lower=exp_lower,
+            exp_upper=exp_upper,
+            max_page=CONFIG['resume_scraping_params']['pages_per_search']
+        )
+        links.append("|".join(tmp_list))
+        # # randomly restart driver
+        # if random.uniform(0,1) < restart_driver_prob:
+        #     driver.close()
+        #     driver = set_up_driver()
+    
+    links_df = grid.copy()
+    links_df["links"] = links
+    output_path = os.path.join(
+        os.getcwd(), 
+        "data", 
+        f"resume_links_{datetime.now().strftime('%Y%m%d%H%M%S')}.csv"
+    )
+    links_df.to_csv(output_path, index=False)
+    return links_df
 
+def resume_link_to_data(links_df):
+
+    scraped_tuples = []
+    for i in tqdm(range(links_df.shape[0]), "Processing group", ncols=100, total=links_df.shape[0]):
+        
+        job_fam = str(links_df['job_family'][i])
+        url_list = str(links_df['links'][i]).split("|")
+        for url in tqdm(url_list, "Scraping resume details", ncols=100, total=len(url_list)):
+            driver = set_up_driver()
+            job_title, resume, years, skill_exp = get_resume_info(driver=driver, link=url)
+            driver.quit()
+            scraped_tuples.append((job_fam, job_title, resume, years, skill_exp))
+    
+    output_df = pd.DataFrame(data=scraped_tuples, columns=["job_family", "job_title", "resume", "years_of_experience", "skills_experience"])
+    output_path = os.path.join(
+        os.getcwd(), 
+        "data", 
+        f"resume_data_{datetime.now().strftime('%Y%m%d%H%M%S')}.csv"
+    )
+    output_df.to_csv(output_path, index=False)
+
+    return output_df
 
 if __name__ == "__main__":
 
     start_time = time.time()
-
-    logger.info("Starting the process")
-    
-    scrape_resume()
-    # # Step 1: extract information from job posts 
-    # logger.info("Extracting info from job posts...")
-    # job_post_df_extracted = extract_job_post_info(
-    #     input_path=JOB_POSTING_PATH,
-    #     model_name=FLAN_T5_MODEL_NAME,
-    # )
-    # logger.info("Extraction from job post finished")
-
-    # output_path = os.path.join(
-    #     os.getcwd(), 
-    #     "data", 
-    #     f"job_posting_transformed_{datetime.now().strftime('%Y%m%d%H%M%S')}.csv"
-    # )
-    
-    # logger.info("Saving job post extraction output to %s" % (output_path))
-    # job_post_df_extracted.to_csv(output_path,index=False)
-
+    df = scrape_resume()
+    resume_df = resume_link_to_data(df)
     end_time = time.time()
 
     logger.info("Processed complete in %s" % (format_execution_time(start_time, end_time)))
